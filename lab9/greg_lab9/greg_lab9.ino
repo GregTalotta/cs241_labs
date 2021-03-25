7a9375cdad31866eca8a628d8c79bb251505d1c6
Gregory Talotta <gjtalotta@alaska.edu>
2021-03-24 19:32:51 -0900

    assignment 2 draft code

--- lab9/greg_lab9/greg_lab9.ino
+++ lab9/greg_lab9/greg_lab9.ino
@@ -22,24 +22,136 @@ void corruptMemory(void *startPointer, int nBytes, long bitErrorRate)
         start[i] ^= (1 << bit); // flip this bit
 }
 
+const int nCommands = 7;
+const String available_commands[nCommands] = {
+    "help\n\tprints all available commands",
+    "Lstr\n\tcorrups local string",
+    "Gstr\n\tcorrupts glibal string",
+    "Larr\n\tcorrupts local array",
+    "Garr\n\tcorrupts global array",
+    "AllG\n\tcorrupts all global variables",
+    "All\n\tcorrupts all stack variables"};
+
+void help()
+{
+  Serial.print("\n");
+  for (int i = 0; i < nCommands; ++i)
+  {
+    Serial.print(available_commands[i] + "\n");
+  }
+  return;
+}
+
+void corrLstr()
+{
+  char *x = "hello\n";
+  corruptMemory(&x, 7, 10000);
+  Serial.println(x);
+  return;
+}
+
+char *gstr = "gello\n";
+void corrGstr()
+{
+  corruptMemory(&gstr, 7, 10000);
+  Serial.println(gstr);
+  return;
+}
+
+void corrLarr()
+{
+  int x[10] = {0};
+  corruptMemory(&x, sizeof(x), 10000);
+  for (int i = 0; i < 5; ++i)
+  {
+    Serial.println(x[i]);
+  }
+  return;
+}
+
+int gx[10] = {0};
+void corrGarr()
+{
+  corruptMemory(&gx, sizeof(gx), 10000);
+  for (int i = 0; i < 5; ++i)
+  {
+    Serial.println(gx[i]);
+  }
+  return;
+}
+
+void corrAllG()
+{
+  void *address = (void *)0x256;
+  corruptMemory(&address, 300, 10000);
+  return;
+}
+
+void corrAll()
+{
+  void *address = (void *)0x2000;
+  corruptMemory(&address, 300, 10000);
+  return;
+}
+void handleCommand(String command)
+{
+  char *ptr1;
+  Serial.println("Received Command: " + command);
+  if (command == "help")
+  {
+    help();
+  }
+  if (command == "Lstr")
+  {
+    corrLstr();
+  }
+  if (command == "Gstr")
+  {
+    corrGstr();
+  }
+  if (command == "Larr")
+  {
+    corrLarr();
+  }
+  if (command == "Garr")
+  {
+    corrGarr();
+  }
+  if (command == "AllG")
+  {
+    corrLarr();
+  }
+  if (command == "All")
+  {
+    corrLarr();
+  }
+  return;
+}
+
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("Enter \"help\" for commands");
+  randomSeed(1234);
 }
-int t = 0;
+
+String buffer;
 void loop()
 {
-  if (t == 0)
+  while (Serial.available() > 0)
   {
-    for (int i = 0; i < 3000; ++i)
-    {
-      unsigned int x = 0;
-      corruptMemory(&x, sizeof(x), 10000);
-      if (x != 0)
-      {
-        Serial.println(x);
-      }
+    int c = Serial.read();
+    if (c < 0)
+      break;
+    if (c == '\n')
+    { // newline, check for a valid command
+      handleCommand(buffer);
+      buffer = "";
     }
-    ++t;
+    else if (c != '\r')
+    { // normal non-control char, just add it to the string
+      buffer += (char)c;
+    }
   }
+  // … do normal work here: blink LEDs, etc
 }
